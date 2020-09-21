268ca603d64528c671b195c48b89b7de2b2a500d
Adrian082818 <mohabiradrian@gmail.com>
2020-09-21 11:21:40 -0400

    testing for loop for coordinates marker

--- map1.py
+++ map1.py
@@ -2,7 +2,11 @@ import folium
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+
+for coordinates in [[38.58, -99.09], [36.58, -97.09]]:
+    fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color='red')))
+
+
 map.add_child(fg) #allows to add layer controll feature later on by creating feature group and then mapping add_child with fg
 
 map.save("Map1.html")
