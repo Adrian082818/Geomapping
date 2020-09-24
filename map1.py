afaf6f6fa821ae1165ea4ddbd1137c31711c8b7c
Adrian082818 <mohabiradrian@gmail.com>
2020-09-24 13:05:25 -0400

    will update for loop for marker locations of volcanoes in US

--- map1.py
+++ map1.py
@@ -2,15 +2,15 @@ import folium
 import pandas
 
 data = pandas.read_csv("Volcanoes.txt")
-lat = list(data["LON"])
-lon = list(data["LAT"])
+lat = list(data["LAT"])
+lon = list(data["LON"])
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[38.58, -99.09], [36.58, -97.09]]:
-    fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color='red')))
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='red')))
 
 
 map.add_child(fg) #allows to add layer controll feature later on by creating feature group and then mapping add_child with fg
