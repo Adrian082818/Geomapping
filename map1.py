4c772e9bbbf0f2e29b9d70292269d69f63677cee
Adrian082818 <mohabiradrian@gmail.com>
2020-10-06 11:13:30 -0400

    will create variable to allow layer control of mapping feature

--- map1.py
+++ map1.py
@@ -1,7 +1,8 @@
 import folium
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
-map.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
-
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+map.add_child(fg) #allows to add layer controll feature later on by creating feature group and then mapping add_child with fg
 
 map.save("Map1.html")
