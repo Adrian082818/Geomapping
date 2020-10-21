30aba9e4ea6e856ea7c99578f7e6cf12d8f1c6f1
Adrian082818 <mohabiradrian@gmail.com>
2020-10-21 12:55:34 -0400

    will change marker to show elevation of location

--- map1.py
+++ map1.py
@@ -4,13 +4,15 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='red')))
+for lt, ln, el in zip(lat, lon, elev):
+    # print(type(el))
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el), icon=folium.Icon(color='red')))
 
 
 map.add_child(fg) #allows to add layer controll feature later on by creating feature group and then mapping add_child with fg
