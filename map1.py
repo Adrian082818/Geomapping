5411f42f2a3adb3bd187d48e633985fe12ca3807
Adrian082818 <mohabiradrian@gmail.com>
2020-10-21 19:17:17 -0400

    will update styling for map to load correctly

--- map1.py
+++ map1.py
@@ -5,24 +5,25 @@ data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
-name = list(data["NAME"])
 
-html = """
-Volcano name:<br>
-<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
-Height: %s m
-"""
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln, el, name in zip(lat, lon, elev, name):
-    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='red')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" m", icon=folium.Icon(color=color_producer(el))))
 
 
 map.add_child(fg) #allows to add layer controll feature later on by creating feature group and then mapping add_child with fg
 
-# map.save("Map1.html")
-map.save("Map_html_popup_advanced.html")
+map.save("Map1.html")
