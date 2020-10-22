62733134149ccfb899577b1e5066117a577507c5
Adrian082818 <mohabiradrian@gmail.com>
2020-10-21 20:18:15 -0400

    seperating volcanoes and popualtion for layer controls

--- map1.py
+++ map1.py
@@ -18,15 +18,19 @@ def color_producer(elevation):
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+" m", fill_color=color_producer(el), color = 'grey', fill_opacity=0.7))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+" m", fill_color=color_producer(el), color = 'grey', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), 
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), 
 style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red' })) #displaying land areas as polygons
 
-map.add_child(fg) #allows to add layer controll feature later on by creating feature group and then mapping add_child with fg
+map.add_child(fgv) #allows to add layer controll feature later on by creating feature group and then mapping add_child with fg
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
